43b8ea812d4e443728123223e0c3e0717e0a9265
linusidom <linusidom@gmail.com>
2022-08-13 06:06:33 +0700

    added Jenkins File

---
+++ Jenkinsfile
@@ -0,0 +1,20 @@
+pipeline {
+  agent any
+  stages {
+    stage('Test'){
+      steps {
+        echo 'Test' 
+      }
+    }
+    stage('Build'){
+      steps {
+        echo 'Build'
+      }
+    }
+    stage('Deploy') {
+      steps { 
+        echo 'Deploy'
+      }
+    }
+  }
+}
--- posts/tests.py
+++ posts/tests.py
@@ -1,22 +1,26 @@
 from django.test import TestCase, LiveServerTestCase, Client
-
+from django.test.testcases import skipIfDBFeature
 from posts.models import Post
 from posts import views
 # Create your tests here.
 
 
 class PostTestCase(LiveServerTestCase):
-    def setUp(self):
-        title = 'new title'
-        message = 'new message'
-        self.data_dict = {'title':title, 'message':message}
-        Post.objects.create(**self.data_dict)
+    # def setUp(self):
+    #     title = 'new title'
+    #     message = 'new message'
+    #     self.data_dict = {'title':title, 'message':message}
+    #     Post.objects.create(**self.data_dict)
 
+    # @skipIfDBFeature('supports_transactions')
+    # def test_posts_can_speak(self):
+    #     """Posts that can speak are correctly identified"""
+    #     new_title = Post.objects.get_queryset().first()
+    #     # print(new_title.title, new_title.message)
 
-    def test_posts_can_speak(self):
-        """Posts that can speak are correctly identified"""
-        new_title = Post.objects.get_queryset().first()
-        # print(new_title.title, new_title.message)
+    #     self.assertEqual(new_title.title, self.data_dict['title'])
 
-        self.assertEqual(new_title.title, self.data_dict['title'])
-
+    def test_posts_load(self):
+        c = Client()
+        response = c.get('/posts/')
+        self.assertEqual(response.status_code, 200)
